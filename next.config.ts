26fe3cff39989cd3be0a75d971a816761ae6d8df
javo-zy <javier.baldizon.12@gmail.com>
2025-07-09 22:42:24 -0600

    Config: Disable ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // --- AÑADE ESTA SECCIÓN ---
+  eslint: {
+    // Advertencia: Esto permite que el proyecto se construya (build)
+    // incluso si tu código tiene errores de ESLint.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
